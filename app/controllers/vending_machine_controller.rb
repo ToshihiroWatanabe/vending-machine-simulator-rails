a016ad60e3c1f3e9ea48ec9b356f33d71807cde6
ToshihiroWatanabe <79039863+ToshihiroWatanabe@users.noreply.github.com>
2021-11-11 14:53:44 +0900

    update: 管理画面とのページ移動を実装

--- app/controllers/vending_machine_controller.rb
+++ app/controllers/vending_machine_controller.rb
@@ -2,4 +2,8 @@ class VendingMachineController < ActionController::Base
   def index
     render action: :index
   end
+
+  def admin
+    render action: :admin
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   root 'vending_machine#index'
+  get 'admin', to: 'vending_machine#admin'
 end
