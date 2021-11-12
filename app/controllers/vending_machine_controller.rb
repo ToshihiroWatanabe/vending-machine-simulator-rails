402d0782ca3939dc90478c8e565ca3ba28cbe005
ToshihiroWatanabe <79039863+ToshihiroWatanabe@users.noreply.github.com>
2021-11-12 17:13:11 +0900

    improve

--- app/controllers/vending_machine_controller.rb
+++ app/controllers/vending_machine_controller.rb
@@ -9,7 +9,6 @@ class VendingMachineController < ActionController::Base
   def insert
     money_type = params[:money_type].to_i
     money_stock = MoneyStock.first
-    puts money_stock.inspect
     # 投入金額が9999円を超えるなら投入しない
     return if money_stock[:deposit] + money_type > 9999
     # 同じ硬貨は20枚を超えて投入できない
@@ -26,16 +25,10 @@ class VendingMachineController < ActionController::Base
     money_stock = MoneyStock.first
     # 一度も商品が購入されていない時
     money_stock[:deposit] = 0
-    money_stock[:stock_10] -= money_stock[:deposit_10]
-    money_stock[:deposit_10] = 0
-    money_stock[:stock_50] -= money_stock[:deposit_50]
-    money_stock[:deposit_50] = 0
-    money_stock[:stock_100] -= money_stock[:deposit_100]
-    money_stock[:deposit_100] = 0
-    money_stock[:stock_500] -= money_stock[:deposit_500]
-    money_stock[:deposit_500] = 0
-    money_stock[:stock_1000] -= money_stock[:deposit_1000]
-    money_stock[:deposit_1000] = 0
+    [10, 50, 100, 500, 1000].each do |money_type|
+      money_stock["stock_#{money_type}".to_sym] -= money_stock["deposit_#{money_type}".to_sym]
+      money_stock["deposit_#{money_type}".to_sym] = 0
+    end
     money_stock.save
     redirect_to '/'
   end
