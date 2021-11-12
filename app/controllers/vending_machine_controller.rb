77ca9cacb84758803d7282f57c12cf64d6563f52
ToshihiroWatanabe <79039863+ToshihiroWatanabe@users.noreply.github.com>
2021-11-12 09:23:25 +0900

    update: 商品ボタンの表示を実装

--- app/controllers/vending_machine_controller.rb
+++ app/controllers/vending_machine_controller.rb
@@ -1,5 +1,7 @@
 class VendingMachineController < ActionController::Base
   def index
+    @item_stocks = ItemStock.joins(product: :temperature)
+                            .select('item_stocks.*, products.name, products.price, temperatures.name AS temperature')
     @money_stock = MoneyStock.first
     render action: :index
   end
