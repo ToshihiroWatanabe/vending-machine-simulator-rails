08b3d7f04bc67ebadc48992b3ed88ac4a8f3b081
ToshihiroWatanabe <79039863+ToshihiroWatanabe@users.noreply.github.com>
2021-11-12 14:45:45 +0900

    update

--- app/controllers/vending_machine_controller.rb
+++ app/controllers/vending_machine_controller.rb
@@ -6,6 +6,22 @@ class VendingMachineController < ActionController::Base
     render action: :index
   end
 
+  def insert
+    puts('insert ' + params[:moneyType])
+    money_stock = MoneyStock.first
+    # 投入金額が9999円を超えるなら投入しない
+    if money_stock[:deposit] + params[:moneyType].to_i > 9999
+      redirect_to '/'
+      return
+    end
+    # 同じ硬貨は20枚を超えて投入できない
+    if money_stock[('stock_' + params[:moneyType].to_s).to_sym] >= 20
+      redirect_to '/'
+      return
+    end
+    redirect_to '/'
+  end
+
   def admin
     render action: :admin
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root 'vending_machine#index'
+  post 'insert/:moneyType', to: 'vending_machine#insert'
   get 'admin', to: 'vending_machine#admin'
 end
