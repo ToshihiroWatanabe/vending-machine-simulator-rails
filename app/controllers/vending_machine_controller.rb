70f56f44efa56d4da5f0d72fd700987cba5cfe43
ToshihiroWatanabe <79039863+ToshihiroWatanabe@users.noreply.github.com>
2021-11-12 16:22:02 +0900

    update: お金の返却を実装

--- app/controllers/vending_machine_controller.rb
+++ app/controllers/vending_machine_controller.rb
@@ -9,18 +9,33 @@ class VendingMachineController < ActionController::Base
   def insert
     money_type = params[:money_type].to_i
     money_stock = MoneyStock.first
-
     puts money_stock.inspect
-
     # 投入金額が9999円を超えるなら投入しない
     return if money_stock[:deposit] + money_type > 9999
-
     # 同じ硬貨は20枚を超えて投入できない
     return if money_stock["deposit_#{money_type}".to_sym] >= 20
 
-    # 更新
     money_stock[:deposit] += money_type
+    money_stock["deposit_#{money_type}".to_sym] += 1
     money_stock["stock_#{money_type}".to_sym] += 1
+    money_stock.save
+    redirect_to '/'
+  end
+
+  def release
+    money_stock = MoneyStock.first
+    # 一度も商品が購入されていない時
+    money_stock[:deposit] = 0
+    money_stock[:stock_10] -= money_stock[:deposit_10]
+    money_stock[:deposit_10] = 0
+    money_stock[:stock_50] -= money_stock[:deposit_50]
+    money_stock[:deposit_50] = 0
+    money_stock[:stock_100] -= money_stock[:deposit_100]
+    money_stock[:deposit_100] = 0
+    money_stock[:stock_500] -= money_stock[:deposit_500]
+    money_stock[:deposit_500] = 0
+    money_stock[:stock_1000] -= money_stock[:deposit_1000]
+    money_stock[:deposit_1000] = 0
     money_stock.save
     redirect_to '/'
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'vending_machine#index'
   post 'insert/:money_type', to: 'vending_machine#insert'
+  post 'release', to: 'vending_machine#release'
   get 'admin', to: 'vending_machine#admin'
 end
